0c23e285dab214df5ecfbe03d270896b1dc2019d
refatahmed441 <refatahmed441@gmail.com>
2024-10-26 16:59:19 +0600

    navbar

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
+import Hader from "../Hader/Hader";
 
 const Root = () => {
   return (
     <div>
+      <Hader></Hader>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
