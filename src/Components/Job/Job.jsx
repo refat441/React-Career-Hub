66e489577701e32b6d42cf1002b1465b284164c7
refatahmed441 <refatahmed441@gmail.com>
2024-10-27 16:53:39 +0600

    up

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -16,8 +16,16 @@ const Job = ({ job }) => {
       <div className="card-body">
         <h2 className="card-title">{job_title}</h2>
         <p>{company_name}</p>
+        <div>
+          <button className="px-5 py-2 font-extrabold border rounded border-[#7R90FE] mr-4">
+            {remote_or_onsite}
+          </button>
+          <button className="px-5 py-2 font-extrabold border rounded border-[#7R90FE] mr-4">
+            {job_type}
+          </button>
+        </div>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary"> View Details </button>
         </div>
       </div>
     </div>
